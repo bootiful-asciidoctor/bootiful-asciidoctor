e3a076681ed121b8883a1ab1460d2df3f126127b
Trisha Gee <trisha.gee@gmail.com>
2022-12-11 15:20:55 +0100

    Updating the optimize process. Instead of having to do the optimisation after producing the PDF, we can now pass the optimize argument to asciidoctor PDF. This will use the PdfOptimizerQuality value set in the properties file.

--- asciidoctor-autoconfiguration/src/main/java/bootiful/asciidoctor/autoconfigure/AbstractPdfProducer.java
+++ asciidoctor-autoconfiguration/src/main/java/bootiful/asciidoctor/autoconfigure/AbstractPdfProducer.java
@@ -2,13 +2,10 @@ package bootiful.asciidoctor.autoconfigure;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.asciidoctor.Asciidoctor;
-import org.springframework.util.FileCopyUtils;
 
 import java.io.File;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 @Log4j2
@@ -44,57 +41,6 @@ abstract class AbstractPdfProducer implements DocumentProducer {
 	}
 
 	/**
-	 * This functionality requires {@code asciidoctor-pdf-optimize} to be on the PATH. See
-	 * <a href=
-	 * "https://asciidoctor.org/docs/asciidoctor-pdf/#optimizing-the-generated-pdf"> this
-	 * documentation on the Asciidoctor PDF optimizer</a>. You must have Ghostscript
-	 * command {@code  gs} and the {@code rghost} gem installed to use it.
-	 */
-	@SneakyThrows
-	@Deprecated
-	protected File[] optimizeOutputPdf(File file) {
-		var configuration = getPdfProducerConfiguration();
-		if (!configuration.isOptimize()) {
-			return new File[] { file };
-		}
-
-		// ok so the original file we get will be overwritten so we need to copy it and
-		// then use the copy to pass to the optimizer
-		var optimizedPdfFile = new File(file.getParentFile(), "index-" + configuration.getMedia() + "-optimized.pdf");
-		if (optimizedPdfFile.exists()) {
-			optimizedPdfFile.delete();
-		}
-
-		bootiful.asciidoctor.autoconfigure.FileCopyUtils.copy(file, optimizedPdfFile);
-
-		if (log.isDebugEnabled()) {
-			log.debug(
-					"pdf.media=" + this.properties.getPdf() + " configuration.getMedia()=" + configuration.getMedia());
-		}
-
-		var process = new ProcessBuilder().command("asciidoctor-pdf-optimize", file.getAbsolutePath()).inheritIO()
-				.directory(file.getParentFile()).start();
-
-		var exitCode = process.waitFor();
-
-		/*
-		 * var process = Runtime.getRuntime() // .exec("asciidoctor-pdf-optimize " +
-		 * file.getAbsolutePath()
-		 *//*
-			 * todo make this work again + " --quality " + configuration.
-			 * getPdfOptimizerQuality().name(). toLowerCase()
-			 *//*
-				 * ); var exitCode = process.waitFor();
-				 */
-		if (log.isDebugEnabled()) {
-			log.debug("exit code: " + exitCode);
-			log.error("error: " + FileCopyUtils.copyToString(new InputStreamReader(process.getErrorStream())));
-			log.debug("input: " + FileCopyUtils.copyToString(new InputStreamReader(process.getInputStream())));
-		}
-		return new File[] { file, optimizedPdfFile };
-	}
-
-	/**
 	 * contains the subclass specific customizations of how the
 	 * {@link AbstractPdfProducer} should do its work.
 	 */
@@ -108,16 +54,6 @@ abstract class AbstractPdfProducer implements DocumentProducer {
 		var regularFile = this.producePdf(media,
 				new File(this.properties.getRoot(), "index-" + media.toLowerCase() + ".pdf"));
 		files.add(regularFile);
-		if (optimize) {
-			File compressedFile = new File(this.properties.getRoot(),
-					"index-optimized-" + media.toLowerCase() + ".pdf");
-			bootiful.asciidoctor.autoconfigure.FileCopyUtils.copy(regularFile, compressedFile);
-
-			optimizeOutputPdf(compressedFile);
-
-			files.add(compressedFile);
-
-		}
 		return files.toArray(new File[0]);
 	}
 
@@ -140,6 +76,12 @@ abstract class AbstractPdfProducer implements DocumentProducer {
 				.attribute("notitle")//
 				.attribute("pdf-stylesdir", pdf.getStyles().getAbsolutePath()) //
 				.attribute("pdf-fontsdir", pdf.getFonts().getAbsolutePath());
+
+		if (this.getPdfProducerConfiguration().isOptimize()) {
+			PdfOptimizerQuality pdfOptimizerQuality = this.getPdfProducerConfiguration().getPdfOptimizerQuality();
+			log.debug(() -> "optimize value = " + pdfOptimizerQuality.toString().toLowerCase());
+			attributesBuilder.attribute("optimize", pdfOptimizerQuality.toString().toLowerCase());
+		}
 
 		var optionsBuilder = this.buildCommonOptions("pdf", attributesBuilder).docType("book").toFile(file);
 		asciidoctor.convertFile(this.getIndexAdoc(this.properties.getRoot()), optionsBuilder);
