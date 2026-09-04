4f4a166ee14503d3fb77c21a132597459c773ef1
Josh Long <joshua.long@broadcom.com>
2026-09-04 14:30:05 +0200

    added file dedupliation to the zip logic

--- asciidoctor-publication-job/src/main/java/bootiful/asciidoctor/files/ZipUtils.java
+++ asciidoctor-publication-job/src/main/java/bootiful/asciidoctor/files/ZipUtils.java
@@ -3,6 +3,8 @@ package bootiful.asciidoctor.files;
 import org.springframework.util.Assert;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
@@ -14,62 +16,66 @@ import java.util.zip.ZipOutputStream;
  */
 public abstract class ZipUtils {
 
-	public static void buildZipFileFromDirectory(File zipFile, File directory) {
-		zip(zipFile, directory, Objects.requireNonNull(directory.listFiles()));
-	}
+    public static void buildZipFileFromDirectory(File zipFile, File directory) {
+        zip(zipFile, directory, Objects.requireNonNull(directory.listFiles()));
+    }
 
-	public static void buildZipFileFromFiles(File zipFile, File[] fileList) {
-		zip(zipFile, getCommonDirectoryFor(fileList), fileList);
-	}
+    private static File[] deduped(File[] fileList) {
+        var dedupeFiles = new HashSet<>(Arrays.asList(fileList));
+        return dedupeFiles.toArray(new File[fileList.length]);
+    }
 
-	private static void zip(File zipFile, File base, File[] fileList) {
-		var buffer = new byte[1024];
-		try (var fos = new FileOutputStream(zipFile); var zos = new ZipOutputStream(fos)) {
-			for (var file : fileList) {
-				var path = file.getAbsolutePath();
-				var basePath = base.getAbsolutePath();
-				Assert.state(path.startsWith(basePath),
-						() -> String.format("the basePath (%s) should be a part of the path (%s)", basePath, path));
-				var ze = new ZipEntry(path.substring(basePath.length()));
-				zos.putNextEntry(ze);
-				try (var in = new FileInputStream(file)) {
-					var len = -1;
-					while ((len = in.read(buffer)) > 0) {
-						zos.write(buffer, 0, len);
-					}
-					zos.flush();
-				}
-			}
-		}
-		catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public static void buildZipFileFromFiles(File zipFile, File[] fileList) {
+        zip(zipFile, getCommonDirectoryFor(fileList), deduped(fileList));
+    }
 
-	private static File getCommonDirectoryFor(File[] files) {
-		var stringsArray = Stream//
-			.of(files)//
-			.map(File::getAbsolutePath)//
-			.toArray(String[]::new);
-		var commonRoot = getLongestCommonPrefix(stringsArray);
-		var root = new File(commonRoot);
-		while (!root.isDirectory()) {
-			root = root.getParentFile();
-		}
-		return root;
-	}
+    private static void zip(File zipFile, File base, File[] fileList) {
+        var buffer = new byte[1024];
+        try (var fos = new FileOutputStream(zipFile); var zos = new ZipOutputStream(fos)) {
+            for (var file : fileList) {
+                var path = file.getAbsolutePath();
+                var basePath = base.getAbsolutePath();
+                Assert.state(path.startsWith(basePath),
+                        () -> String.format("the basePath (%s) should be a part of the path (%s)", basePath, path));
+                var ze = new ZipEntry(path.substring(basePath.length()));
+                zos.putNextEntry(ze);
+                try (var in = new FileInputStream(file)) {
+                    var len = -1;
+                    while ((len = in.read(buffer)) > 0) {
+                        zos.write(buffer, 0, len);
+                    }
+                    zos.flush();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	private static String getLongestCommonPrefix(String[] s) {
-		var k = s[0].length();
-		for (var i = 1; i < s.length; i++) {
-			k = Math.min(k, s[i].length());
-			for (var j = 0; j < k; j++)
-				if (s[i].charAt(j) != s[0].charAt(j)) {
-					k = j;
-					break;
-				}
-		}
-		return s[0].substring(0, k);
-	}
+    private static File getCommonDirectoryFor(File[] files) {
+        var stringsArray = Stream//
+                .of(files)//
+                .map(File::getAbsolutePath)//
+                .toArray(String[]::new);
+        var commonRoot = getLongestCommonPrefix(stringsArray);
+        var root = new File(commonRoot);
+        while (!root.isDirectory()) {
+            root = root.getParentFile();
+        }
+        return root;
+    }
+
+    private static String getLongestCommonPrefix(String[] s) {
+        var k = s[0].length();
+        for (var i = 1; i < s.length; i++) {
+            k = Math.min(k, s[i].length());
+            for (var j = 0; j < k; j++)
+                if (s[i].charAt(j) != s[0].charAt(j)) {
+                    k = j;
+                    break;
+                }
+        }
+        return s[0].substring(0, k);
+    }
 
 }
